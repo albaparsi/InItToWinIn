ffeda293a950dc83d5c87d166617e4a9781b8ecf
albaparsi <146134287+albaparsi@users.noreply.github.com>
2024-03-19 16:32:06 -0400

    Add files via upload

---
+++ InItToWinIt/InItToWinIt/InItToWinIt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/* The purpose of this program is to  first show the picked game which is karaoke in this case, then to
+show the name of the picked song for karaoke, the original singer of the song, and to show the duration of the song.
+*/
+int main() {
+
+	std::cout << "Karaoke\n";
+	std::cout << "Wow\n";
+	//The Wow song writers: Austin Post, Louis Bell, Adam Feeney, Billy Walsh, Anthonie Walters, CarlmRosen.
+	std::cout << "As performed by: Post Malone" << endl;
+	std::cout << "Time: 2:30" << endl;
+
+	return 0;
+}
